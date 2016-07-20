f4e2b4cbab58f78f4ee500d320d7a8bd2f034a8a
Brandon Haymore <bhaymore@local-motors.com>
2016-07-20 10:43:55 -0700

    working copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,16 @@ apt-get install -y apache2 php5 mysql-server php5-mysql php5-gd php5-apcu php5-i
 
 mysql -uroot -p$dbrootpass -e "create database osticket;create user osticket@localhost identified by 'password';grant all privileges on osticket.* to osticket@localhost"
 
-tar -xzf /vagrant/v1.10-rc.3.tar.gz -C /tmp
+tar -xzf /vagrant/files/v1.10-rc.3.tar.gz -C /tmp
 
 cp -R /tmp/osTicket-1.10-rc.3/* /var/www/html/
-cp /var/www/html/include/ost-sampleconfig.php /var/www/html/include/ost-config.php
+cp /vagrant/files/ost-config.php /var/www/html/include/ost-config.php
 chown www-data. /var/www/html/include/ost-config.php
 
 rm /var/www/html/index.html
+
+rm -rf /var/www/html/setup/
+
+gunzip -c /vagrant/files/osticket.sql.gz | mysql -uosticket -ppassword osticket
 
 service apache2 restart
